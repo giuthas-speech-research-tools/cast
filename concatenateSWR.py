b37ddfa484bb0b9383e01a909380ac74d098ef38
Pertti Palo <giuthas@gmail.com>
2022-06-25 01:58:05 +0300

    Wav is now generated correctly.
    Work on textgrid generation is still on going.
    Also made several corrections of arguments being mistaken for each other.

--- concatenateSWR.py
+++ concatenateSWR.py
@@ -59,27 +59,24 @@ def read_pronunciation_dict(filename):
     if os.path.isfile(filename):
         with closing(open(filename, 'r')) as csvfile:
             reader = csv.reader(csvfile, delimiter='\t')
-            pronunciation_dict = {row[0]: row[1:] for row in reader}
+            pronunciation_dict = {row[0]: list(filter(None, row[1:])) for row in reader}
         return pronunciation_dict
     else:
         print("Didn't find {pronunciation_dict}. Exiting.")
         sys.exit()
 
 
-def write_concatenated_textgrid(table, filename, dirname):
-    print(table)
-    print(filename)
-    print("textgrids does not yet support all the operations we need. work on that.")
+def write_concatenated_textgrid(table, filename, pronunciation_dict_name):
+    pronunciation_dict = read_pronunciation_dict(pronunciation_dict_name)
 
-    pronunciation_dict = read_pronunciation_dict(os.path.join(dirname, 
-                                                'pronunciation_dict.csv'))
-
+    transcription_table = {}
     for entry in table:
         print("Processing {word} which is pronounced {transcription}.", 
             word = entry['word'], transcription = pronunciation_dict[entry['word']])
 
-        # Add transcription info to the table entry
         # Run beep detect on the wav.
+
+        # Add transcription info to the table entry
         # Add timing info to the table entry.
 
     # Possibly after transforming the table into another list of dicts
@@ -104,41 +101,43 @@ def read_na_list(dirname):
 
 
 def processWavFile(table_entry, wav_file, filename, prompt_file, uti_file, 
-                    na_list, output, samplerate, number_of_channels, cursor):
+                    na_list, samplerate, number_of_channels, cursor):
     if filename in na_list:
-        print('Skipping {filename}: Token is in na_list.txt.', filename=filename)
-        return
+        print('Skipping {filename}: Token is in na_list.txt.'.format(filename=filename))
+        return cursor, None
     elif not os.path.isfile(uti_file):
-        print ('Skipping {filename}. Token has no ultrasound data.', 
-            filename=filename)
-        return
+        print ('Skipping {filename}. Token has no ultrasound data.'.format(filename=filename))
+        return cursor, None
         
     with closing(open(prompt_file, 'r')) as prompt_file:
         line = prompt_file.readline().strip()
         line = " ".join(re.findall("[a-zA-Z]+", line))
-        if line == 'water swallow' or line == 'BITE PLANE':
-            print('Skipping {file} is a {prompt}', file=prompt_file, prompt=line)
-            return
+        if line == 'tap test' or line == 'water swallow' or line == 'biteplate':
+            print('Skipping {file} is a {prompt}'.format(file=prompt_file, prompt=line))
+            return cursor, None
 
         table_entry['word'] = line
 
     (next_samplerate, frames) = sio_wavfile.read(wav_file)
     n_frames = frames.shape[0]
-    n_channels = frames.shape[1]
+    if len(frames.shape) == 1:
+        n_channels = 1
+    else:
+        n_channels = frames.shape[1]
 
     if next_samplerate != samplerate:
         print('Mismatched sample rates in sound files.')
-        print("{next_samplerate} in {infile} is not the common one: {samplerate}", 
-            next_samplerate=next_samplerate, infile=wav_file, samplerate=samplerate)
+        print("{next_samplerate} in {infile} is not the common one: {samplerate}".format(
+            next_samplerate=next_samplerate, infile=wav_file, samplerate=samplerate))
         print('Exiting.')
-        exit()
+        sys.exit()
 
     if n_channels != number_of_channels:
         print('Mismatched numbers of channels in sound files.')
-        print("{n_channels} in {infile} is not the common one: {number_of_channels}", 
-            n_channels=n_channels, infile=wav_file, number_of_channels=number_of_channels)
+        print("{n_channels} in {infile} is not the common one: {number_of_channels}".format( 
+            n_channels=n_channels, infile=wav_file, number_of_channels=number_of_channels))
         print('Exiting.')
-        exit()
+        sys.exit()
 
     duration = n_frames / float(samplerate)
 
@@ -153,17 +152,14 @@ def processWavFile(table_entry, wav_file, filename, prompt_file, uti_file,
     cursor += duration
     table_entry['end'] = round(cursor, 3)
 
-    # TODO: does this actually append rather than overwrite?
-    sio_wavfile.write(output, samplerate, frames)
-
-    return cursor
+    return cursor, frames
 
 
-def concatenateWavs(dirname, outfilename, speaker_id):
-    wav_files = glob.glob(os.path.join(dirname, '*.wav')) 
+def concatenateWavs(speaker_id, dirname, pronunciation_dict_name, outfilename):
+    wav_files = sorted(glob.glob(os.path.join(dirname, '*.wav'))) 
 
     if(len(wav_files) < 1):
-        print("Didn't find any sound files to concatanate in \'"+dirname+"\'.")
+        print("Didn't find any sound files to concatanate in \'{dirname}\'.".format(dirname))
         exit()
 
     # Split first to get rid of the suffix, then to get rid of the path.
@@ -183,7 +179,7 @@ def concatenateWavs(dirname, outfilename, speaker_id):
                 'word':'n/a'} 
              for i, wavfile in  enumerate(wav_files)]
 
-    prompt_files = glob.glob(os.path.join(dirname, '*.txt')) 
+    prompt_files = sorted(glob.glob(os.path.join(dirname, '*.txt'))) 
 
     if(len(prompt_files) < 1):
         print("Didn't find any prompt files.")
@@ -204,37 +200,53 @@ def concatenateWavs(dirname, outfilename, speaker_id):
                  for filename in filenames]
     
     # find params from first file
-    (samplerate, test_data) = sio_wavfile.read(wav_files[0])
-    number_of_channels = test_data.shape[1]
+    samplerate, test_data = sio_wavfile.read(wav_files[0])
+    if len(test_data.shape) == 1:
+        number_of_channels = 1
+    else:
+        number_of_channels = test_data.shape[1]
 
+    # Read wavs and keep track of file boundaries.
     cursor = 0.0
-    with closing(outwave, 'w') as output:
-        for i in range(len(wav_files)):
-            cursor = processWavFile(table[i], wav_files[i], filenames[i], 
-                    prompt_files[i], uti_files[i], 
-                    na_list, output, samplerate, number_of_channels, cursor)
+    frames = None
+    for i in range(len(wav_files)):
+        cursor, new_frames = processWavFile(table[i], wav_files[i], filenames[i], 
+                prompt_files[i], uti_files[i], 
+                na_list, samplerate, number_of_channels, cursor)
+        if new_frames is None:
+            continue
+        if frames is None:
+            frames = new_frames
+        else:
+            frames = np.concatenate([frames, new_frames], axis = 0)
+
+    # Write the concatanated wav.
+    with closing(open(outwave, 'wb')) as output:
+        sio_wavfile.write(output, samplerate, frames)
 
     # Weed out the skipped ones before writing the data out.
     table = [token for token in table if token['id'] != 'n/a']
     write_fav_input(table, outfav)
     write_results(table, outcsv)
-    write_concatenated_textgrid(table, out_textgrid, dirname)
+    write_concatenated_textgrid(table, out_textgrid, pronunciation_dict_name)
     #pp.pprint(table)
 
 
 def main(args):
     outfilename = args.pop()
+    pronunciation_dict_name = args.pop()
     original_dirname = args.pop()
     speaker_id = args.pop()
-    concatenateWavs(speaker_id, original_dirname, outfilename)
+    concatenateWavs(speaker_id, original_dirname, pronunciation_dict_name, outfilename)
 
 
 if (len(sys.argv) != 4):
     print("\nex1_concat.py")
-    print("\tusage: ex1_concat.py speaker_id original_directory outputfilename")
+    print("\tusage: ex1_concat.py speaker_id original_directory pronunciation_dict_name outputfilename")
     print("\n\tConcatenates wav files from AAA.")
     print("\tWrites a huge wav-file and a .txt for potential input to FAVE.")
-    print("\tAlso writes a richer metafile to be read by ex1_extract.py or similar.")
+    print("\tAlso writes a richer metafile to be read by extract_swr.py or similar.")
+    print("\tAlso writes a huge textgrid with phonological transcriptions of the words.")
     sys.exit(0)
 
 
