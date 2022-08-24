8a666b48e6481865459e6a0c575870ba4b2b7fad
Pertti Palo <giuthas@gmail.com>
2022-08-25 01:38:56 +0300

    Working extraction script.

--- extractSWR.py
+++ extractSWR.py
@@ -4,7 +4,8 @@ import os
 import pprint
 import sys
 import time
-from textgrids import *
+
+import textgrids
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -19,123 +20,48 @@ def read_results(filename):
     return table
 
 
-def write_results(table, filename):
-    # Dump all the metadata into a csv-formated file to
-    # be read by Python or R.
-    with closing(open(filename, 'w')) as csvfile:
-        fieldnames = ['id', 'speaker', 'sliceBegin', 'beep', 'begin', 'end', 'word']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writeheader()
-        map(writer.writerow, table)
-
-    print("Wrote file " + filename + " for R/Python.")
-
-
-def writeGrid(grid, element, directory):
-    filename = os.path.join(directory, element['id'])+'.TextGrid'
-    grid.write(filename)
-    #print 'Wrote', filename
-
-
-def gridFromDict(element):
-    grid = TextGrid(name = element['speaker'], 
-                    maxTime = element['end']-element['sliceBegin'])
-
-    word = IntervalTier('word', maxTime = grid.maxTime)
-    grid.append(word)
-
-    segment = IntervalTier('segment', maxTime = grid.maxTime)
-    segment.add(0.0, float(element['beep']), '')
-    grid.append(segment)
-
-    return grid
-
-
-def loadTransRules(filename):
-    rules = {}
-    with closing(open(filename, 'r')) as rulefile:
-        for rule in rulefile:
-            rule = rule.strip().split(',')
-            rules[rule[0]] = rule[1:]
-    return rules
-
-
-def extractGrids(table, mainGrid):
-    grids = map(gridFromDict, table)
-    words = mainGrid.pop()
-    segments = mainGrid.pop()
-
-    transcriptions = loadTransRules("/Users/jpalo/tyot/vaikkari/source/python/phone_transcription_rules.csv")
+def extractGrids(table, mainGrid, directory):
+    utterances = mainGrid.interval_tier_to_array("Utterance")
+    words = mainGrid.interval_tier_to_array("Word")
+    segments = mainGrid.interval_tier_to_array("Segments")
+    details = mainGrid.interval_tier_to_array("Phonetic detail")
 
     i = 0
-    for interval in words:
-        if interval.mark != 'sp':
-            interval.mark = interval.mark.lower()
-            interval.move(-float(table[i]['sliceBegin']))       
-            tier = grids[i].getFirst('word')
-            
-            if interval.minTime > tier.maxTime:
-                print("Segmentation error: %s.", interval)
-            elif interval.maxTime > tier.maxTime:
-                print("Warning word interval exceeds recording length.")
-                print("\tRecording length: %f.", tier.maxTime) 
-                print("\tInterval: %s", interval)
-                tier.add(interval.minTime, tier.maxTime, interval.mark)
-            else: 
-                tier.addInterval(interval)
-                tier.add(interval.maxTime, tier.maxTime, "")
-            # advance the counter for only those intervals that contain 
-            # segmentation information
-            i += 1
+    for entry in table:
+        utterance = []
+        word = []
+        segment = []
+        detail = []
 
-    i = 0
-    seg_iter = segments.__iter__()
-    lastBoundary = 0.0
-    for interval in seg_iter:
-        if 'sp' not in interval.mark:
-            offset = -float(table[i]['sliceBegin'])
-            tier = grids[i].getFirst('segment')
+        for interval in utterances:
+            if interval['begin'] >= entry["sliceBegin"] and interval['end'] <= entry["sliceEnd"]:
+                utterance.append(interval)
+ 
+        for interval in words:
+            if interval['begin'] >= entry["sliceBegin"] and interval['end'] <= entry["sliceEnd"]:
+                word.append(interval)
+ 
+        for interval in segments:
+            if interval['begin'] >= entry["sliceBegin"] and interval['end'] <= entry["sliceEnd"]:
+                segment.append(interval)
+  
+        for interval in details:
+            if interval['begin'] >= entry["sliceBegin"] and interval['end'] <= entry["sliceEnd"]:
+                detail.append(interval)
 
-#            rule = iter(transcriptions[table[i]['word']])
+        textgrid = textgrids.TextGrid(xmin = entry["sliceBegin"])
+        textgrid.interval_tier_from_array("utterance", utterance)
+        textgrid.interval_tier_from_array("word", word)
+        textgrid.interval_tier_from_array("segment", segment)
+        textgrid.interval_tier_from_array("Phonetic detail", detail)
 
-            interval.move(offset)
-            interval.mark = transcriptions[interval.mark][0]
-            if interval.minTime > float(table[i]['beep']):
-                print("offset: %f, %d", -offset, i)
-                tier.add(float(table[i]['beep']), interval.minTime, 'beep')
-            else:
-                interval = seg_iter.next()
-                while 'sp' not in interval.mark:
-                    # advance the segment intervals 
-                    # until you find an empty interval
-                    try:
-                        interval = seg_iter.next()
-                    except StopIteration:
-                        break
-                i += 1
-                continue
-            if interval.maxTime > tier.maxTime:
-                interval.maxTime = tier.maxTime
-            lastBoundary = interval.maxTime
-            tier.addInterval(interval)
+        textgrid.move_t0(-entry["sliceBegin"])
 
-            interval = seg_iter.next()
-            while 'sp' not in interval.mark:
-                interval.move(offset)
-                if interval.maxTime > tier.maxTime:
-                    interval.maxTime = tier.maxTime
-                interval.mark = transcriptions[interval.mark][0]
-                lastBoundary = interval.maxTime
-                tier.addInterval(interval)
-                try:
-                    interval = seg_iter.next()
-                except StopIteration:
-                    break
-            tier.add(lastBoundary, tier.maxTime, "")
-            i += 1
+        filename = os.path.join(directory, entry['id'])+'.TextGrid'
+        textgrid.write(filename)
+        i += 1
 
-    return grids, transcriptions
+    return i
 
 
 def main(args):
@@ -144,20 +70,20 @@ def main(args):
     gridFile = args.pop()
 
     table = read_results(resultsFile)
-    mainGrid = TextGridFromFile(gridFile)
-    grids, transcriptions = extractGrids(table, mainGrid)
 
-    dirnames = [outdirectory]*len(grids)
-    map(writeGrid, grids, table, dirnames)
-    print('Wrote %s textgrids.', str(len(grids)))
+    mainGrid = textgrids.TextGrid(gridFile)
+    print(f"Read {gridFile}.")
+
+    i = extractGrids(table, mainGrid, outdirectory)
+    print(f'Wrote {i} textgrids.')
 
 
 if (len(sys.argv) != 4):
-    print("\nex2_fav_extract.py")
-    print("\tusage: ex2_fav_extract.py fave_output.textgrid previous_results.csv output_dir")
-    print("\n\tExtracts single token textgrids from FAVE output textgrid.")
-    print("\tReads also analysis results produced by ex2_concat.py.")
-    print("\tWrites the extracted textgrids with added beep locations to the specified directory.")
+    print("\nextractSWR.py")
+    print("\tusage: extractSWR.py concatenated.textgrid previous_results.csv output_dir")
+    print("\n\tExtracts single token textgrids from a concatenated textgrid produced by FAVE or concatenateSWR.py.")
+    print("\tReads also analysis results produced by concatenateSWR.py.")
+    print("\tWrites the extracted textgrids to the specified directory.")
     sys.exit(0)
 
 
