a70b7d4b5ce8f2235e7a26a4d86022f221effaec
Pertti Palo <giuthas@gmail.com>
2022-10-04 18:11:17 -0400

    Fixes to yaml processing and reports to the user.

--- cast/config_file_io.py
+++ cast/config_file_io.py
@@ -17,13 +17,13 @@ def read_config_file(filepath: Path=None) -> Dict:
     if filepath is None:
         filepath = Path('cast_config.yml')
 
-    if filepath.isfile():
+    if filepath.is_file():
         with closing(open(filepath, 'r')) as yaml_file:
             config_dict = strictyaml.load(yaml_file.read())
     else:
-        print("Didn't find {filepath}. Exiting.", str(filepath))
+        print(f"Didn't find {filepath}. Exiting.".format(str(filepath)))
         sys.exit()
-    return config_dict
+    return config_dict.data
 
 
 def read_exclusion_list(filepath: Path) -> Dict:
@@ -33,13 +33,14 @@ def read_exclusion_list(filepath: Path) -> Dict:
     If no exclusion list file is present, return an empty array
     after warning the user.
     """
-    if filepath.isfile():
+    if filepath.is_file():
         with closing(open(filepath, 'r')) as yaml_file:
             exclusion_dict = strictyaml.load(yaml_file.read())
     else:
         exclusion_dict = {}
-        print("Did not find the exclusion list. Proceeding anyhow.")
-    return exclusion_dict
+        print(f"Did not find the exclusion list at {filepath}. Proceeding anyhow.".format( 
+                str(filepath)))
+    return exclusion_dict.data
 
 
 def read_na_list(dirpath: Path) -> List:
@@ -50,7 +51,7 @@ def read_na_list(dirpath: Path) -> List:
     after warning the user.
     """
     na_file = dirpath.joinpath('na_list.txt')
-    if na_file.isfile():
+    if na_file.is_file():
         na_list = [line.rstrip('\n') for line in open(na_file)]
     else:
         na_list = []
@@ -69,11 +70,11 @@ def read_pronunciation_dict(filepath: Path) -> Dict:
     Returns a dict where each entry is a list of phonomes.
     """
     pronunciation_dict = {}
-    if filepath.isfile():
+    if filepath.is_file():
         with closing(open(filepath, 'r')) as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
             pronunciation_dict = {row[0]: list(filter(None, row[1:])) for row in reader}
         return pronunciation_dict
     else:
-        print("Didn't find {pronunciation_dict}. Exiting.", pronunciation_dict)
+        print(f"Didn't find {pronunciation_dict}. Exiting.".format(pronunciation_dict))
         sys.exit()
