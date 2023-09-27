152e99e819a6788ec0c36e79ebb650f94a8b0316
Pertti Palo <giuthas@gmail.com>
2023-09-27 02:27:57 -0400

    Style updates.

--- cast/concatenate.py
+++ cast/concatenate.py
@@ -1,7 +1,7 @@
 #
 # Copyright (c) 2022-2023 Pertti Palo.
 #
-# This file is part of Computer Assisted Segmentation Tools 
+# This file is part of Computer Assisted Segmentation Tools
 # (see https://github.com/giuthas-speech-research-tools/cast/).
 #
 # This program is free software: you can redistribute it and/or modify
@@ -45,8 +45,9 @@ from cast import (check_and_load_aaa_meta, check_and_load_csv_meta,
 
 pp = pprint.PrettyPrinter(indent=4)
 
-def process_wav_file(table_entry: dict, samplerate: float, number_of_channels: int, cursor: float, 
-                    high_pass_filter: Union[dict[str, np.ndarray], None]=None) -> Tuple[float, np.ndarray]:
+
+def process_wav_file(table_entry: dict, samplerate: float, number_of_channels: int, cursor: float,
+                     high_pass_filter: Union[dict[str, np.ndarray], None] = None) -> Tuple[float, np.ndarray]:
     (next_samplerate, frames) = sio_wavfile.read(table_entry['wav_path'])
     n_frames = frames.shape[0]
     if len(frames.shape) == 1:
@@ -57,7 +58,7 @@ def process_wav_file(table_entry: dict, samplerate: float, number_of_channels: i
     if next_samplerate != samplerate:
         print('Mismatched sample rates in sound files.')
         print("{next_samplerate} in {infile} is not the common one: {samplerate}".format(
-            next_samplerate=next_samplerate, infile=table_entry['wav_file'], 
+            next_samplerate=next_samplerate, infile=table_entry['wav_file'],
             samplerate=samplerate))
         print('Exiting.')
         sys.exit()
@@ -65,7 +66,7 @@ def process_wav_file(table_entry: dict, samplerate: float, number_of_channels: i
     if n_channels != number_of_channels:
         print('Mismatched numbers of channels in sound files.')
         print("{n_channels} in {infile} is not the common one: {number_of_channels}".format(
-            n_channels=n_channels, infile=table_entry['wav_file'], 
+            n_channels=n_channels, infile=table_entry['wav_file'],
             number_of_channels=number_of_channels))
         print('Exiting.')
         sys.exit()
@@ -83,13 +84,14 @@ def process_wav_file(table_entry: dict, samplerate: float, number_of_channels: i
         table_entry['has speech'] = has_speech
         # Start segmentation in FAV and other systems after the beep.
         table_entry['begin'] = cursor + beep + 0.05
-    else: 
+    else:
         table_entry['begin'] = cursor
     cursor += duration
     table_entry['end'] = round(cursor, 3)
     table_entry['sliceEnd'] = cursor
 
     return cursor, frames
+
 
 def apply_exclusion_list(table: list[dict], exclusion_path: Path) -> None:
 
@@ -104,23 +106,23 @@ def apply_exclusion_list(table: list[dict], exclusion_path: Path) -> None:
             print(f'Excluding {filename}: File is in exclusion list.')
             entry['excluded'] = True
 
-        # The first condition sees if the whole prompt is excluded, 
-        # the second condition checks if any parts of the prompt 
-        # match exclucion criteria (for example excluding 'foobar ...' 
+        # The first condition sees if the whole prompt is excluded,
+        # the second condition checks if any parts of the prompt
+        # match exclucion criteria (for example excluding 'foobar ...'
         # based on 'foobar').
         prompt = entry['prompt']
         if (prompt in exclusion_list['prompts'] or
-            [element for element in exclusion_list['parts of prompts'] if(element in prompt)]):
-            print(f'Excluding {filename}. Prompt: {prompt} matches exclusion list.')
+                [element for element in exclusion_list['parts of prompts'] if (element in prompt)]):
+            print(
+                f'Excluding {filename}. Prompt: {prompt} matches exclusion list.')
             entry['excluded'] = True
 
 
-
-def concatenate_wavs(speaker_id: str, directory: Union[str, Path], 
-                        outputfile: Union[str, Path], config_dict: dict, 
-                        pronunciation_dict: Union[dict, None]=None, 
-                        test: bool=False, detect_beep: bool=False, only_words: bool=False, 
-                        csv_meta_file: Optional[Path]=None):
+def concatenate_wavs(speaker_id: str, directory: Union[str, Path],
+                     outputfile: Union[str, Path], config_dict: dict,
+                     pronunciation_dict: Union[dict, None] = None,
+                     test: bool = False, detect_beep: bool = False, only_words: bool = False,
+                     csv_meta_file: Optional[Path] = None):
     if isinstance(directory, str):
         directory = Path(directory)
     if isinstance(outputfile, str):
@@ -132,7 +134,8 @@ def concatenate_wavs(speaker_id: str, directory: Union[str, Path],
     elif data_source == 'RASL':
         table = check_and_load_rasl_meta(speaker_id, directory, test)
     elif data_source == 'csv':
-        table = check_and_load_csv_meta(speaker_id, directory, test, csv_meta_file)
+        table = check_and_load_csv_meta(
+            speaker_id, directory, test, csv_meta_file)
     else:
         print(f"Unknown data source: {data_source}. Exiting.")
         sys.exit()
@@ -142,12 +145,12 @@ def concatenate_wavs(speaker_id: str, directory: Union[str, Path],
     # Only add the beep entry if we are going to be using it.
     if detect_beep:
         for entry in table:
-            entry['beep'] = 'n/a' 
+            entry['beep'] = 'n/a'
 
     outwave = outputfile.with_suffix(".wav")
     outcsv = outputfile.with_suffix(".csv")
     out_textgrid = outputfile.with_suffix(".TextGrid")
-    
+
     # find params from first file
     samplerate, test_data = sio_wavfile.read(table[0]['wav_path'])
     if len(test_data.shape) == 1:
@@ -159,7 +162,8 @@ def concatenate_wavs(speaker_id: str, directory: Union[str, Path],
     # TODO: consider moving the whole loop into processWavFile and renaming the function
     if detect_beep:
         mains_frequency = 60
-        high_pass_filter = audio_processing.high_pass(samplerate, mains_frequency)
+        high_pass_filter = audio_processing.high_pass(
+            samplerate, mains_frequency)
 
     cursor = 0.0
     frames = None
@@ -167,17 +171,18 @@ def concatenate_wavs(speaker_id: str, directory: Union[str, Path],
         if entry['excluded']:
             continue
         if detect_beep:
-            cursor, new_frames = process_wav_file(entry, samplerate, 
-                    number_of_channels, cursor, high_pass_filter=high_pass_filter)
+            cursor, new_frames = process_wav_file(
+                entry, samplerate, number_of_channels, cursor,
+                high_pass_filter=high_pass_filter)
         else:
-            cursor, new_frames = process_wav_file(entry, samplerate, 
-                    number_of_channels, cursor)
+            cursor, new_frames = process_wav_file(
+                entry, samplerate, number_of_channels, cursor)
         if new_frames is None:
             continue
         if frames is None:
             frames = new_frames
         else:
-            frames = np.concatenate([frames, new_frames], axis = 0)
+            frames = np.concatenate([frames, new_frames], axis=0)
 
     # Write the concatanated wav.
     with closing(open(outwave, 'wb')) as output:
@@ -191,4 +196,3 @@ def concatenate_wavs(speaker_id: str, directory: Union[str, Path],
     else:
         generate_textgrid(table, out_textgrid, config_dict, pronunciation_dict)
     # pp.pprint(table)
-
--- cast/textgrid_functions.py
+++ cast/textgrid_functions.py
@@ -1,7 +1,7 @@
 #
 # Copyright (c) 2022-2023 Pertti Palo.
 #
-# This file is part of Computer Assisted Segmentation Tools 
+# This file is part of Computer Assisted Segmentation Tools
 # (see https://github.com/giuthas-speech-research-tools/cast/).
 #
 # This program is free software: you can redistribute it and/or modify
@@ -42,7 +42,10 @@ pp = pprint.PrettyPrinter(indent=4)
 # def add_boundaries_and_segments:
 
 # TODO: chop the rewrite to pieces. try starting with the config file and the pseudocode
-def add_tiers(textgrid: TextGrid, table: List, config_dict: Dict, pronunciation_dict: Dict=None) -> None:
+
+
+def add_tiers(textgrid: TextGrid, table: List, config_dict: Dict,
+              pronunciation_dict: Dict = None) -> None:
     """
     Add Tiers to a TextGrid
 
@@ -61,37 +64,47 @@ def add_tiers(textgrid: TextGrid, table: List, config_dict: Dict, pronunciation_
     end_coeff = config_dict['word guess']['end']
 
     if config_dict['flags']['utterance'] or config_dict['flags']['word']:
-        textgrid.interval_tier_from_array(config_dict['tier names']['word'], words)
+        textgrid.interval_tier_from_array(
+            config_dict['tier names']['word'], words)
     if config_dict['flags']['phoneme']:
-        textgrid.interval_tier_from_array(config_dict['tier names']['phoneme'], segments)
+        textgrid.interval_tier_from_array(
+            config_dict['tier names']['phoneme'], segments)
     if config_dict['flags']['phone']:
-        textgrid.interval_tier_from_array(config_dict['tier names']['phone'], segments)
+        textgrid.interval_tier_from_array(
+            config_dict['tier names']['phone'], segments)
 
     for entry in table:
         if pronunciation_dict:
             if entry['prompt'] in pronunciation_dict:
                 transcription = pronunciation_dict[entry['prompt']]
-                print(f"Generating boundaries for {entry['prompt']} which is pronounced {transcription}.")
+                print(
+                    f"Generating boundaries for {entry['prompt']} which is pronounced {transcription}.")
                 entry['transcription'] = transcription
 
-                # Generate an evenly spaced first guess of segmentation by taking the 
-                # middle third of the potential speech interval and chopping it up. 
-                earliest_speech = entry['begin'] +.058
-                seg_begin = earliest_speech + (entry['end'] - earliest_speech)*begin_coeff
-                seg_end = earliest_speech + (entry['end'] - earliest_speech)*end_coeff
-                boundaries = np.linspace(seg_begin, seg_end, len(transcription) + 3)
+                # Generate an evenly spaced first guess of segmentation by taking the
+                # middle third of the potential speech interval and chopping it up.
+                earliest_speech = entry['begin'] + .058
+                seg_begin = earliest_speech + \
+                    (entry['end'] - earliest_speech)*begin_coeff
+                seg_end = earliest_speech + \
+                    (entry['end'] - earliest_speech)*end_coeff
+                boundaries = np.linspace(
+                    seg_begin, seg_end, len(transcription) + 3)
                 boundaries = boundaries[1:-1]
                 entry['segment boundaries'] = boundaries
             else:
-                print(f"Word \'{entry['prompt']}\' missing from pronunciation dict.")
+                print(
+                    f"Word \'{entry['prompt']}\' missing from pronunciation dict.")
         else:
             print(f"Generating boundaries for {entry['prompt']}.")
 
-            earliest_speech = entry['begin'] +.058
-            seg_begin = earliest_speech + (entry['end'] - earliest_speech)*begin_coeff
-            seg_end = earliest_speech + (entry['end'] - earliest_speech)*end_coeff
+            earliest_speech = entry['begin'] + .058
+            seg_begin = earliest_speech + \
+                (entry['end'] - earliest_speech)*begin_coeff
+            seg_end = earliest_speech + \
+                (entry['end'] - earliest_speech)*end_coeff
             boundaries = [seg_begin, seg_end]
-            entry['segment boundaries'] = boundaries    
+            entry['segment boundaries'] = boundaries
 
 
 def generate_textgrid(table, filename, config_dict, pronunciation_dict=None) -> None:
@@ -118,50 +131,50 @@ def generate_textgrid(table, filename, config_dict, pronunciation_dict=None) -> 
     segments = []
     for entry in table:
         file_segment = {
-            'label': entry['filename'], 
-            'begin': entry['sliceBegin'], 
+            'label': entry['filename'],
+            'begin': entry['sliceBegin'],
             'end': entry['sliceEnd']}
         files.append(file_segment)
 
         if 'beep' in entry:
             begin_buffer = {
-                'label': '', 
-                'begin': entry['sliceBegin'], 
+                'label': '',
+                'begin': entry['sliceBegin'],
                 'end': entry['beep']
-                }
+            }
             words.append(begin_buffer)
             segments.append(begin_buffer)
 
             beep = {
-                'label': 'BEEP', 
-                'begin': entry['beep'], 
+                'label': 'BEEP',
+                'begin': entry['beep'],
                 'end': entry['beep'] + 0.05
-                }
+            }
             words.append(beep)
             segments.append(beep)
 
             after_beep = {
-                'label': '', 
-                'begin': entry['beep'] + 0.05, 
+                'label': '',
+                'begin': entry['beep'] + 0.05,
                 'end': entry['segment boundaries'][0]
-                }
-            words.append(after_beep)          
-            segments.append(after_beep)          
+            }
+            words.append(after_beep)
+            segments.append(after_beep)
         else:
             begin_buffer = {
-                'label': '', 
-                'begin': entry['sliceBegin'], 
+                'label': '',
+                'begin': entry['sliceBegin'],
                 'end': entry['segment boundaries'][0]
-                }
+            }
             words.append(begin_buffer)
             segments.append(begin_buffer)
 
         # For words and utterances one long interval.
         word = {
-            'label': entry['prompt'], 
-            'begin': entry['segment boundaries'][0], 
+            'label': entry['prompt'],
+            'begin': entry['segment boundaries'][0],
             'end': entry['segment boundaries'][-1]
-            }
+        }
         words.append(word)
 
         if pronunciation_dict:
@@ -175,8 +188,8 @@ def generate_textgrid(table, filename, config_dict, pronunciation_dict=None) -> 
                 segments.append(segment)
 
         end_buffer = {
-            'label': '', 
-            'begin': entry['segment boundaries'][-1], 
+            'label': '',
+            'begin': entry['segment boundaries'][-1],
             'end': entry['sliceEnd']}
         words.append(end_buffer)
         segments.append(end_buffer)
@@ -189,11 +202,15 @@ def generate_textgrid(table, filename, config_dict, pronunciation_dict=None) -> 
     if config_dict['flags']['file']:
         textgrid.interval_tier_from_array("File", files)
     if config_dict['flags']['utterance']:
-        textgrid.interval_tier_from_array(config_dict['tier names']['utterance'], words)
+        textgrid.interval_tier_from_array(
+            config_dict['tier names']['utterance'], words)
     if config_dict['flags']['word']:
-        textgrid.interval_tier_from_array(config_dict['tier names']['word'], words)
+        textgrid.interval_tier_from_array(
+            config_dict['tier names']['word'], words)
     if config_dict['flags']['phoneme']:
-        textgrid.interval_tier_from_array(config_dict['tier names']['phoneme'], segments)
+        textgrid.interval_tier_from_array(
+            config_dict['tier names']['phoneme'], segments)
     if config_dict['flags']['phone']:
-        textgrid.interval_tier_from_array(config_dict['tier names']['phone'], segments)
+        textgrid.interval_tier_from_array(
+            config_dict['tier names']['phone'], segments)
     textgrid.write(filename)
